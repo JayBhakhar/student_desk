062afca6af21f564459f29f6b44a38528c7d4a34
Jay Bhakhar <jaybhakhar@gmail.com>
2021-12-04 22:09:07 +0500

    moongoos

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -12,7 +13,22 @@ app.set('view engine', 'ejs');
 app.use(express.static("public")); // folder name :- public
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+mongoose.connect("mongodb+srv://JayBhakhar:jay456789@student-desk.ff0hu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true});
+
+const studentSchema = {
+    name: String,
+    faculty: String,
+    course: String,
+    group: String
+  };
+
+const Student = mongoose.model("Student", studentSchema);
+
 app.get("/", (req, res) => {
+    Student.find({}, function(err, student){
+        console.log(student);
+    });
     res.render("dashboard");
 });
 
@@ -21,16 +37,19 @@ app.get("/add_student", (req, res) => {
 });
 
 
-app.post("/add_student", (req, res) => {
-    console.log('post');
-    console.log(req.body.name);
-    console.log(req.body.faculty);
-    console.log(req.body.course);
-    console.log(req.body.group);
-        // name = req.body.name,
-        // faculty = req.body.faculty,
-        // course = req.body.course,
-        // group = req.body.group,    
+app.post("/add_student", (req, res) => {    
+    const student = new Student({
+        name: req.body.name,
+        faculty: req.body.faculty,
+        course: req.body.course,
+        group: req.body.group
+      });  
+    
+    student.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+    });       
 });
 
 app.listen(3000, function() {
