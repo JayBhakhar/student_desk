f722eb43598c8dfea918097b334a2401254f383c
Jay Bhakhar <jaybhakhar@gmail.com>
2021-12-04 16:24:51 +0500

    html to ejs

--- index.js
+++ index.js
@@ -3,15 +3,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const ejs = require("ejs");
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(express.static("public")); // folder name :- public
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/dashboard.html');
+    res.render("dashboard");
+});
+
+app.get("/add_student", (req, res) => {
+    res.render("add_student");
 });
 
 app.listen(3000, function() {
