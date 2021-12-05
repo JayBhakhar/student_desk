0e1bf4c87f1fa9f386fb9d0678c7667f10359211
Jay Bhakhar <jaybhakhar@gmail.com>
2021-12-05 18:54:46 +0500

    port change

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ app.post("/add_student", (req, res) => {
     });       
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("App listening on port 3000!");
 });
